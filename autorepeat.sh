cd187b3bed9be852c711095800aa3893f615ac6a
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2013-10-04 15:49:22 +0200

    Included trap for ctrl+c to kill remaining instances and avoided error on wrong nb of args.

--- autorepeat.sh
+++ autorepeat.sh
@@ -5,25 +5,32 @@
 APPLICATION=$1
 SLEEPTIME=$2
 RUNTIMES=$3
+PID=$$
 
-echo "Pid of this parent: $$"
-echo $$
-echo "Starting application."
+trap 'echo "`basename $0`: Ouch! Quitting early." 1>&2 && echo "Emergency kill in progress..." && kill $PID 1>&2 >/dev/null && k=1' 1 2 15
 
-c=1
-while [ $c -le $RUNTIMES ]
-do
-    echo "Count: $c "
-    (( c++ ))
+case $# in
+    3)  echo "Pid of this parent: $$"
+        echo $$
+        echo "Starting application."
+
+        c=1 
+        k=0
+        while [ $c -le $RUNTIMES ] && [ $k -ne 1 ]
+        do
+            echo "Count: $c "
+            (( c++ ))
         
-    $APPLICATION &
-    PID=$!
-    echo "Pid of child is: $PID"
-    #echo $PID
-    sleep $SLEEPTIME
-    echo "Killing application."
-    kill $PID >/dev/null
-done
+            $APPLICATION &
+            PID=$!
+            echo "Pid of child is: $PID"
+            #echo $PID
+            sleep $SLEEPTIME
+            echo "Killing application."
+            kill $PID >/dev/null
+        done
 
-echo "Script done."
-
+        echo "Script done."
+    ;;
+    *)  echo "Usage: `basename $0` [Applicationpath] [Time to wait before killing in seconds] [number of times to run]" 1>&2 ;;
+esac
